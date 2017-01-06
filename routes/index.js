e9c754535ac16ff64c0bc7c47b259cbfed63e0d4
abysmli <abysmli@gmail.com>
2017-01-06 13:09:15 +0800

    add custom menu

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var nodeWeixinAuth = require('node-weixin-auth');
+var nodeWeixinMenu = require('node-weixin-menu');
+var config = require("node-weixin-config");
 var settings = require('node-weixin-settings');
 var errors = require('web-errors').errors;
 var request = require('supertest');
@@ -10,6 +12,7 @@ var app = {
   secret: '391dbdd46e663f8e71f53850f6ce1585',
   token: 'allhaha'
 };
+config.app.init(app);
 
 // var app = {
 //   id: 'wx616276dfaab6b39d',
@@ -35,7 +38,6 @@ nodeWeixinAuth.determine(settings, app, function () {
 nodeWeixinAuth.ips(settings, app, function (error, data) {
   //error == false
   //data.ip_list获取IP列表
-  console.log(data.ip_list);
 });
 
 /* GET home page. */
@@ -65,5 +67,63 @@ router.get('/wx/auth/ack', function (req, res) {
     }
   });
 });
+
+
+
+
+var menu = {
+  "button": [
+    {
+      "type": "view",
+      "name": "Allhaha官网",
+      "url": "http://www.soso.com/"
+    },
+    {
+      "name": "功能菜单",
+      "sub_button": [
+        {
+          "type": "click",
+          "name": "扫一扫",
+          "url": "http://www.soso.com/"
+        },
+        {
+          "type": "view",
+          "name": "EAN录入",
+          "url": "http://v.qq.com/"
+        },
+        {
+          "type": "view",
+          "name": "EAN查询",
+          "url": "http://v.qq.com/"
+        }
+      ]
+    }
+  ]
+};
+
+nodeWeixinMenu.create(app, menu, function (error, data) {
+  //error === true
+  //data.errcode === 0
+  //data.errmsg === 'ok'
+});
+
+// nodeWeixinMenu.get(app, function (error, data) {
+//   //error === true
+//   //typeof data.menu
+//   //typeof data.menu.button
+// });
+
+// nodeWeixinMenu.customize(app, function (error, data) {
+//   //error === true
+//   //data.is_menu_open === 1
+//   //data.selfmenu_info
+//   //data.selfmenu_info.button
+// });
+
+// nodeWeixinMenu.remove(app, function (error, data) {
+//   //error === true
+//   //data.errcode
+//   //data.errmsg
+// });
 
 module.exports = router;
