b57b2dfaf0add99d5827ff950ff4396acafa7061
Li, Yuan <abysmli@gmail.com>
2017-07-23 00:41:31 +0200

    add test search support

--- routes/index.js
+++ routes/index.js
@@ -16,20 +16,20 @@ var app = {
 
 nodeWeixinConfig.app.init(app);
 
-nodeWeixinSettings.registerSet(function() {});
+nodeWeixinSettings.registerSet(function () { });
 
-nodeWeixinSettings.registerGet(function() {});
+nodeWeixinSettings.registerGet(function () { });
 
 // 调整TIME_GAP来避免重复请求
 // 默认是500秒，基本上不会出现失效的情况
 nodeWeixinAuth.TIME_GAP = 60;
 
 //手动得到accessToken
-nodeWeixinAuth.tokenize(nodeWeixinSettings, app, function(error, json) {
+nodeWeixinAuth.tokenize(nodeWeixinSettings, app, function (error, json) {
   var accessToken = json.access_token;
 });
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.send("Hello! Willcome to Allhaha website weixin API！");
 });
 
@@ -48,7 +48,7 @@ var menu = {
   }]
 };
 
-nodeWeixinMenu.create(nodeWeixinSettings, app, menu, function(error, data) {
+nodeWeixinMenu.create(nodeWeixinSettings, app, menu, function (error, data) {
   //data.errcode === 0
   //data.errmsg === 'ok'
 });
@@ -56,11 +56,12 @@ nodeWeixinMenu.create(nodeWeixinSettings, app, menu, function(error, data) {
 
 
 // 微信服务器返回的ack信息是HTTP的GET方法实现的
-router.get('/auth/ack', function(req, res) {
+router.get('/auth/ack', function (req, res) {
   var data = nodeWeixinAuth.extract(req.query);
-  nodeWeixinAuth.ack(app.token, data, function(error, data) {
+  nodeWeixinAuth.ack(app.token, data, function (error, data) {
     if (!error) {
       res.send(data);
+      console.log(data);
       return;
     }
     switch (error) {
@@ -78,7 +79,7 @@ router.get('/auth/ack', function(req, res) {
 });
 
 //在http请求里的处理方式
-router.post('/auth/ack', function(req, res) {
+router.post('/auth/ack', function (req, res) {
   var reply = nodeWeixinMessage.reply;
   var message = req.body;
   console.log("Message: ");
@@ -94,13 +95,13 @@ router.post('/auth/ack', function(req, res) {
 
       console.time("HTTPRequest:");
       var eanCode = scanCodes[1].length == 12 ? "0" + scanCodes[1] : scanCodes[1];
-      request('http://allhaha.com/weixin/prerequest?value=' + eanCode + '&from=' + message.xml.FromUserName + '&type=barcode', function(error, response, body) {
+      request('http://allhaha.com/weixin/prerequest?value=' + eanCode + '&from=' + message.xml.FromUserName + '&type=barcode', function (error, response, body) {
         console.timeEnd("HTTPRequest:");
 
-        nodeWeixinAuth.tokenize(nodeWeixinSettings, app, function(error, json) {
+        nodeWeixinAuth.tokenize(nodeWeixinSettings, app, function (error, json) {
           var accessToken = json.access_token;
           console.log(accessToken);
-          request('https://api.weixin.qq.com/cgi-bin/user/info?access_token=' + accessToken + '&openid=' + message.xml.FromUserName + '&lang=zh_CN', function(error, response, body) {
+          request('https://api.weixin.qq.com/cgi-bin/user/info?access_token=' + accessToken + '&openid=' + message.xml.FromUserName + '&lang=zh_CN', function (error, response, body) {
             var result = JSON.parse(body);
             console.log(result);
           });
@@ -113,9 +114,10 @@ router.post('/auth/ack', function(req, res) {
           console.log(result);
           if (result.Result == 'success') {
             console.log('Success');
-            var description = result.Brand === "" ? "" : '品牌： ' + result.Brand + '\n';
+            var description = result.TitleCN === "" ? "" : result.TitleCN + '\n';
+            description += result.Brand === "" ? "" : '品牌： ' + result.Brand + '\n';
             description += result.Price === "" ? "" : '参考价格： ' + result.Price + ' 欧元\n';
-            description += '产品EAN代码： ' + eanCode;
+            description += '产品条码： ' + eanCode;
             var news = reply.news(message.xml.ToUserName, message.xml.FromUserName, [{
               title: result.Title,
               description: description,
@@ -134,12 +136,60 @@ router.post('/auth/ack', function(req, res) {
     } else if (message.xml.Event == 'subscribe') {
       var text = reply.text(message.xml.ToUserName, message.xml.FromUserName, '欢迎来到"欧哈哈"购物王国，欧哈哈，为您发现最低！\n"欧哈哈"比价网www.allhaha.com为欧洲第一款全中文商品比价网站。齐全的商品信息，中文的介绍评论，清晰的价格对比，简明的购物链接，"欧哈哈"为您打造在欧洲最便捷的中文比价服务和线上购物体验。在这里，您可以了解到欧洲任何一款产品最低的价格信息和购买链接；在这里，只为您发现最低。当然，我们的微信公众号“欧哈哈”也同步上线，并于新春佳节推出了"扫码比价"功能，欧洲所有商品一扫即出，中文的服务，便捷的操作，价格一目了然。\n现我们推出新年大抽奖，请扫如下活动码进入商品页面，点击用户评论即可参与赢取该商品活动。');
       return res.send(text);
+    } else if (message.xml.MsgType == 'text') {
+      var content = message.xml.Content;
+      if (content.substring(0, 3) == "EAN") {
+        var eanCode = content.substring(3, content.length);
+        request('http://allhaha.com/weixin/prerequest?value=' + eanCode + '&from=' + message.xml.FromUserName + '&type=barcode', function (error, response, body) {
+          console.timeEnd("HTTPRequest:");
+          nodeWeixinAuth.tokenize(nodeWeixinSettings, app, function (error, json) {
+            var accessToken = json.access_token;
+            console.log(accessToken);
+            request('https://api.weixin.qq.com/cgi-bin/user/info?access_token=' + accessToken + '&openid=' + message.xml.FromUserName + '&lang=zh_CN', function (error, response, body) {
+              var result = JSON.parse(body);
+              console.log(result);
+            });
+          });
+          if (!error && response.statusCode == 200) {
+            var result = JSON.parse(body);
+            console.log("Result: ");
+            console.log(result);
+            if (result.Result == 'success') {
+              console.log('Success');
+              var description = result.TitleCN === "" ? "" : result.TitleCN + '\n';
+              description += result.Brand === "" ? "" : '品牌： ' + result.Brand + '\n';
+              description += result.Price === "" ? "" : '参考价格： ' + result.Price + ' 欧元\n';
+              description += '产品条码： ' + eanCode;
+              var news = reply.news(message.xml.ToUserName, message.xml.FromUserName, [{
+                title: result.Title,
+                description: description,
+                picUrl: result.Image,
+                url: 'http://allhaha.com/weixin/ean?value=' + eanCode + '&from=' + message.xml.FromUserName + '&type=barcode',
+              }]);
+              return res.send(news);
+            } else {
+              console.log('Failed');
+              var text = reply.text(message.xml.ToUserName, message.xml.FromUserName, "您搜索的产品（条形码：" + eanCode + "）还未入库，我们将优先添加！更多问题请联系客服微信：allhaha_com");
+              return res.send(text);
+            }
+          }
+        });
+      }
+
+      //  Message:
+      // Jul 23 06:24:21 Allhaha-Server www[28799]: { xml:
+      // Jul 23 06:24:21 Allhaha-Server www[28799]:    { ToUserName: 'gh_5cf18e9abdeb',
+      // Jul 23 06:24:21 Allhaha-Server www[28799]:      FromUserName: 'o61jbwe0tBB0hJSP-60S4WDgHcWU',
+      // Jul 23 06:24:21 Allhaha-Server www[28799]:      CreateTime: '1500762261',
+      // Jul 23 06:24:21 Allhaha-Server www[28799]:      MsgType: 'text',
+      // Jul 23 06:24:21 Allhaha-Server www[28799]:      Content: '睡觉就睡觉',
+      // Jul 23 06:24:21 Allhaha-Server www[28799]:      MsgId: '6445724830496886237' } }
     } else {
       var text = reply.text(message.xml.ToUserName, message.xml.FromUserName, "请扫描产品条码！");
       return res.send(text);
     }
   } else {
-    var text = reply.text(message.xml.ToUserName, message.xml.FromUserName, "请扫描产品条码！");
+    var text = reply.text(message.xml.ToUserName, message.xml.FromUserName, "请扫描产品条码...");
     return res.send(text);
   }
 });
